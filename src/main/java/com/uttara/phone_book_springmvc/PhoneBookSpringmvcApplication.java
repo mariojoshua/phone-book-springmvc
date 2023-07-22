f8be726ff532f1402be19b19399977be449e134d
MarioJA <mariojoshua.ind@gmail.com>
2023-07-22 12:13:43 +0530

    first working commit

---
+++ src/main/java/com/uttara/phone_book_springmvc/PhoneBookSpringmvcApplication.java
@@ -0,0 +1,14 @@
+package com.uttara.phone_book_springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PhoneBookSpringmvcApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(PhoneBookSpringmvcApplication.class, args);
+    }
+
+}
