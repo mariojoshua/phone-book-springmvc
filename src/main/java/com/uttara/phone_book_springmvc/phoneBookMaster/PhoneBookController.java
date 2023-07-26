845ce2412db2cbef5372a0dc0d991d9f3504c04f
MarioJA <mariojoshua.ind@gmail.com>
2023-07-26 19:55:12 +0530

    Working delete button

--- src/main/java/com/uttara/phone_book_springmvc/phoneBookMaster/PhoneBookController.java
+++ src/main/java/com/uttara/phone_book_springmvc/phoneBookMaster/PhoneBookController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.reactive.result.view.RedirectView;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -57,10 +59,11 @@ public class PhoneBookController {
             return "phoneBookMaster/delete";
         }
 
-        @PostMapping("/delete")
-        public ModelAndView deletePhoneBookMaster(@ModelAttribute PhoneBookMaster phoneBookMaster) {
-        System.out.println(phoneBookMaster.getName() + "result"); 
-            phoneBookMasterService.delete(phoneBookMaster.getName());
+        @PostMapping(value = "delete/{name}")
+        @ResponseBody
+        public ModelAndView deletePhoneBookMaster(@PathVariable("name") String name) {
+        System.out.println(name + "result"); 
+            phoneBookMasterService.delete(name);
         return new ModelAndView("redirect:/phoneBookMasters");
     }
     
